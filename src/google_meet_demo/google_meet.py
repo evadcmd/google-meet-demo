e982cf91c3b6f34964aa7bd736abb6fc69326830
minta.chiu <minta.chu@labbase.co.jp>
2024-12-19 00:39:17 +0900

    fix an undefined parameter

--- src/google_meet_demo/google_meet.py
+++ src/google_meet_demo/google_meet.py
@@ -4,7 +4,7 @@ SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
 CLIENT_CREDENTIALS_FILE = "credentials.json"
 
-def get_google_oauth_flow(redirect_uri: str):
+def get_google_oauth_flow(redirect_uri: str) -> Flow:
     return Flow.from_client_secrets_file(
         CLIENT_CREDENTIALS_FILE, scopes=SCOPES, redirect_uri=redirect_uri
     )
