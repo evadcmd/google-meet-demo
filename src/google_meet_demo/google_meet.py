84b17572e6bf0c30e15834ee2cca117bffb5fbe4
minta.chiu <minta.chu@labbase.co.jp>
2024-12-19 01:04:44 +0900

    fix scopes

--- src/google_meet_demo/google_meet.py
+++ src/google_meet_demo/google_meet.py
@@ -1,10 +1,19 @@
+import asyncio
+
 from google_auth_oauthlib.flow import Flow
 
-SCOPES = ["https://www.googleapis.com/auth/calendar"]
+SCOPES = [
+    "https://www.googleapis.com/auth/calendar",
+    "https://www.googleapis.com/auth/calendar.events",
+]
 
 CLIENT_CREDENTIALS_FILE = "credentials.json"
 
-def get_google_oauth_flow(redirect_uri: str) -> Flow:
+def _get_google_oauth_flow_async(redirect_uri: str) -> Flow:
     return Flow.from_client_secrets_file(
         CLIENT_CREDENTIALS_FILE, scopes=SCOPES, redirect_uri=redirect_uri
     )
+
+async def get_google_oauth_flow(redirect_uri: str) -> Flow:
+    loop = asyncio.get_running_loop()
+    return await loop.run_in_executor(None, _get_google_oauth_flow_async, redirect_uri)
--- src/google_meet_demo/router/auth.py
+++ src/google_meet_demo/router/auth.py
@@ -9,18 +9,18 @@ router = APIRouter(prefix="", tags=["google OAuth endpoint"])
 
 @router.get(path="/")
 async def google_oauth(req: Request):
-    flow = google_meet.get_google_oauth_flow(req.base_url)
+    flow = await google_meet.get_google_oauth_flow(req.base_url)
     if not req.query_params:
-        authorization_url, state = flow.authorization_url(
+        authorization_url, _state = flow.authorization_url(
             access_type="offline", prompt="consent"
         )
         return RedirectResponse(authorization_url)
     else:
         flow.fetch_token(authorization_response=str(req.url))
-        # flow.credentials would be the token
-        service = build("calendar", "v3", credentials=flow.credentials)
+        # flow.credentials is the token
+        calendar = build("calendar", "v3", credentials=flow.credentials)
         events_result = (
-            service.events().list(calendarId="primary", maxResults=10).execute()
+            calendar.events().list(calendarId="primary", maxResults=10).execute()
         )
         events = events_result.get("items", [])
         return {"events": events}
